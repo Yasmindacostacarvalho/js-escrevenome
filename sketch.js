53f68bb6fd5642ad060469be30a2ef20bb6d32bd
Yasmin da Costa Carvalho <169084707+Yasmindacostacarvalho@users.noreply.github.com>
2024-05-07 08:10:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(600, 600);
+  background( "white")
+}
+
+function draw() {
+
+    stroke("blue");
+    fill("red");
+
+
+
+ if (mouseIsPressed) {
+     rect(mouseX, mouseY, 20, 35);
   }
-  
-  function draw() {
-    background(220);
-  }
+}
